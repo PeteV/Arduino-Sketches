2078a929fa513e3c8c8366b8daf253778aaac854
PeteV <PeteV@users.noreply.github.com>
2016-08-27 03:37:56 -0400

    Update and rename Star-Strobe.ino.ino to Star-Strobe.ino

--- Star-Strobe.ino
+++ Star-Strobe.ino
@@ -4,7 +4,7 @@
  * 
  * 
  * Features:
- * Landing light controlled via BLE
+ * Landing lights 
  * 2 fading anti-collision beacons
  * Double flash strobe
  * 
@@ -18,9 +18,6 @@
 #define LL_IRQ_NUMBER 0 // Interrupt number to use (0 = pin 2 on most boards)
 #define LL_PIN_SERVO 2 // Servo input pin number - this needs to match whatever interrupt is used
 #define LL_PIN_LIGHT 3 // Landing light output pin number
-#define LL_SERVO_THRESHOLD 1500 // Servo signal threshold to turn on/off landing light (pulse width in microseconds, 1000 to 2000)
-#define LL_SERVO_DEAD_ZONE 100 // Servo signal dead-zone size, eliminates flicker
-#define LL_SERVO_REVERSED true   // Whether or not the servo channel is reversed
 
 // Strobe Init
 #define STB_PIN_LIGHT 4 // Pin number for strobe light output
@@ -35,8 +32,7 @@
 
 
 // 
-volatile unsigned long servoPulseStartTime;
-volatile int servoPulseWidth = 0;
+
 boolean curLandingLight = false;
 
 unsigned long lastFadeTime = 0;
@@ -46,8 +42,7 @@ int fadeDirection = 1;
 
 void setup()
 {
-  // Set up interrupt handler
- // attachInterrupt(LL_IRQ_NUMBER, measureServoSignal, CHANGE);
+  
 
   // Declare output pins
   pinMode(LL_PIN_LIGHT, OUTPUT);
